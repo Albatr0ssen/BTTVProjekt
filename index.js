d695e85f3f889bdca5943688d72f00bf23c7ba86
Albatr0ssen <axel.berg@jotunheim.se>
2021-09-25 00:21:58 +0200

    oawgjw

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
+const poolConfig = require('./config.js')
+app.use(express.json({limit: '1mb'}));
+app.use(express.urlencoded({extended: true}))
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: poolConfig.host,
+    user: poolConfig.user,
+    password: poolConfig.password,
+    database: poolConfig.database
+});
 
 app.use("/", express.static("public/index"))
 
+app.get("/db", (req, res) => {
+    pool.query("SELECT * FROM test", (err, response) => {
+        if(err){ return console.log('error: ' + err.message) }
+        res.send(response);
+    })
+})
+
 app.listen(port, () => {
-    console.log("Port open at 3000")
+    console.log("HTTP Port Open (Port 3000)")
 })
