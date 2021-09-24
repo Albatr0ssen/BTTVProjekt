87f3c04b454f8e0f1eacded1a0125f59d1ffb7ab
Albatr0ssen <axel.berg@jotunheim.se>
2021-09-25 00:22:46 +0200

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = poolConfig.port;
 const mysql = require('mysql');
 const poolConfig = require('./config.js')
 app.use(express.json({limit: '1mb'}));
