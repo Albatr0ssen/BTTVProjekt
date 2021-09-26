3acd150fe53d0b7ea446197ce7b4e5e7f7647a5b
Axel Berg <axel.berg@jotunheim.se>
2021-09-26 16:00:36 +0200

    gafw

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = poolConfig.port;
 const mysql = require('mysql');
 const poolConfig = require('./config.js')
+const port = poolConfig.port;
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({extended: true}))
 var pool = mysql.createPool({
@@ -15,10 +15,43 @@ var pool = mysql.createPool({
 
 app.use("/", express.static("public/index"))
 
-app.get("/db", (req, res) => {
-    pool.query("SELECT * FROM test", (err, response) => {
+app.get("/emote", (req, res) => {
+    let random = [Math.floor(Math.random() * 485) + 1, Math.floor(Math.random() * 485) + 1];
+    while(random[1] == random[0]){
+        random[1] = Math.floor(Math.random() * 485) + 1;
+    }
+    pool.query(`SELECT emoteName, emoteURL FROM emotes WHERE emoteID IN ( ${random[0]}, ${random[1]} )`, (err, response) => {
         if(err){ return console.log('error: ' + err.message) }
-        res.send(response);
+        res.send(response)
+    })
+})
+
+app.get("/db", (req, res) => {
+    pool.query("SELECT * FROM emotes", (err, response) => {
+        if(err){ return console.log('error: ' + err.message) }
+        // let index = 0;
+        // response.forEach(element => {
+        //     index++;
+        //     pool.query(`UPDATE emotes SET emoteID = null WHERE emoteID = ` + element.emoteID + ` `, (err, response) => {
+        //         if(err){ return console.log('error: ' + err.message) }
+        //         console.log(response.emoteID)
+        //     })
+        // });
+        // res.sendStatus(200);
+
+        // let big = [];
+        // response.forEach(emote1 => {
+        //     response.forEach(emote2 => {
+        //         if(emote1.emoteName == emote2.emoteName && emote1.emoteID != emote2.emoteID){
+        //             console.log(emote1.emoteID, emote2.emoteID)
+        //             big.push({
+        //                 "emote1": emote1,
+        //                 "emote2": emote2,
+        //             })
+        //         }
+        //     });
+        // });
+        // res.send(big);
     })
 })
 
---
+++ public/index/app.js
@@ -0,0 +1,34 @@
+GetEmote();
+
+async function GetEmote(){
+    let emote;
+    await fetch('/emote').then(async res => {
+        await res.json().then(res => {
+            emote = res;
+        })
+    })
+    console.log(emote);
+    
+    for (let index = 0; index <= 1; index++) {
+        document.querySelector(`div[emote="${index}"]`).innerHTML = `
+        <img src="${emote[index].emoteURL}" alt="${emote[index].emoteName}">
+        <span>${emote[index].emoteName}</span>
+        `
+        emote[index].image = new Image();
+        emote[index].image.onload = EmoteLoaded(index, emote);
+        emote[index].image.src = emote[index].emoteURL;
+    }
+}
+
+function EmoteLoaded(index, emote){
+    emote[index].loaded = true;
+    console.log(emote[0].loaded, emote[1].loaded)
+    if(emote[0].loaded && emote[1].loaded){
+        console.log(emote[0].loaded, emote[1].loaded)
+        document.querySelector('div[loading]').remove();
+        document.querySelector('body').classList.remove("loading");
+        document.querySelector('body').classList.add("active");
+        document.querySelector('div[emote="0"]').classList.remove("hidden");
+        document.querySelector('div[emote="1"]').classList.remove("hidden");
+    }
+}
