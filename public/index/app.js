c7f1a4df5f03beb7f5448e8827fbf69c66b8aa46
Axel Berg <axel.berg@jotunheim.se>
2021-10-21 23:58:07 +0200

    no weird image delay

--- public/index/app.js
+++ public/index/app.js
@@ -19,8 +19,8 @@ function EmotesSetup(){
     document.querySelector('div[emote="1"]').classList.remove("hidden");
     document.querySelector('div[emote="2"]').classList.remove("hidden");
     HTMLAddon();
-    // ClickEmoteEventListener(1);
-    // ClickEmoteEventListener(2);
+    ClickEmoteEventListener(1);
+    ClickEmoteEventListener(2);
     ButtonEvents();
 }
 
@@ -32,15 +32,27 @@ async function GetEmote(){
             emote = res.emotes;
         })
     })  
+    let images = [[false, ""], [false, ""]]
     for (let index = 0; index <= 1; index++) {
-        document.querySelector(`div[emote="${index + 1}"]`).innerHTML = `
+        let emoteCard = `
         <div class="emote-background-dark">
             <div class="emote-background center">
                 <img class="emote-img .btn" src="${emote[index].emoteURL}" alt="${emote[index].emoteName}">
                 <span>${emote[index].emoteName}</span>
             </div>
         </div>   
-        `
+        `        
+        let img1 = new Image();
+        img1.onload = () => {  images[index][0] = true; images[index][1] = emoteCard; LoadEmotes(images); }
+        img1.src = emote[index].emoteURL;
+    }
+}
+
+function LoadEmotes(images){
+    console.log(images[0][0], images[1][0])
+    if(images[0][0] == true && images[1][0] == true){
+        document.querySelector(`div[emote="1"]`).innerHTML = images[0][1];
+        document.querySelector(`div[emote="2"]`).innerHTML = images[1][1];
     }
 }
 
@@ -49,19 +61,19 @@ function ClickEmoteEventListener(choiceID){
 }
 
 function PostEmote(choiceID){
-    fetch("postEmote",
-    {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify({
-            "sessionID": sessionID,
-            "userID": UserID(),
-            "emoteChoice": choiceID
-        })
-    })
+    // fetch("postEmote",
+    // {
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //         "sessionID": sessionID,
+    //         "userID": UserID(),
+    //         "emoteChoice": choiceID
+    //     })
+    // })
     GetEmote();
 }
 
